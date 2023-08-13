2418ddff5a804a6afb906420f35a433cd7218464
netra-sawad <100144757+netra-sawad@users.noreply.github.com>
2023-08-13 21:33:57 +0545

    Update VirtualPowerplantSystemApplication.java

--- src/main/java/com/virtual/powerplant/VirtualPowerplantSystemApplication.java
+++ src/main/java/com/virtual/powerplant/VirtualPowerplantSystemApplication.java
@@ -29,31 +29,31 @@ public class VirtualPowerplantSystemApplication {
         return () -> Optional.of(LocalDateTime.now(ZoneOffset.UTC));
     }
 
-    @Bean
-    public CommandLineRunner dataInitializer(BatteryService batteryService) {
-        return args -> {
+//     @Bean
+//     public CommandLineRunner dataInitializer(BatteryService batteryService) {
+//         return args -> {
 //            batteryService.deleteAll();
 
-            var batteryRequests = List.of(
-                    new BatteryRequest("Cannington", "6107", 13500),
-                    new BatteryRequest("Midland", "6057", 50500),
-                    new BatteryRequest("Hay Street", "6000", 23500),
-                    new BatteryRequest("Mount Adams", "6525", 12000),
-                    new BatteryRequest("Koolan Island", "6733", 10000),
-                    new BatteryRequest("Armadale", "6992", 25000),
-                    new BatteryRequest("Kalamunda", "6076", 13500),
-                    new BatteryRequest("Carmel", "6076", 36000),
-                    new BatteryRequest("Bentley", "6102", 85000),
-                    new BatteryRequest("Akunda Bay", "2084", 13500),
-                    new BatteryRequest("University of Melbourne", "3010", 85000),
-                    new BatteryRequest("Norfolk Island", "2899", 13500),
-                    new BatteryRequest("Kent Town", "5067", 13500),
-                    new BatteryRequest("Northgate Mc", "9464", 13500),
-                    new BatteryRequest("Gold Coast Mc", "9729", 50000)
-            );
+//             var batteryRequests = List.of(
+//                     new BatteryRequest("Cannington", "6107", 13500),
+//                     new BatteryRequest("Midland", "6057", 50500),
+//                     new BatteryRequest("Hay Street", "6000", 23500),
+//                     new BatteryRequest("Mount Adams", "6525", 12000),
+//                     new BatteryRequest("Koolan Island", "6733", 10000),
+//                     new BatteryRequest("Armadale", "6992", 25000),
+//                     new BatteryRequest("Kalamunda", "6076", 13500),
+//                     new BatteryRequest("Carmel", "6076", 36000),
+//                     new BatteryRequest("Bentley", "6102", 85000),
+//                     new BatteryRequest("Akunda Bay", "2084", 13500),
+//                     new BatteryRequest("University of Melbourne", "3010", 85000),
+//                     new BatteryRequest("Norfolk Island", "2899", 13500),
+//                     new BatteryRequest("Kent Town", "5067", 13500),
+//                     new BatteryRequest("Northgate Mc", "9464", 13500),
+//                     new BatteryRequest("Gold Coast Mc", "9729", 50000)
+//             );
 
-            batteryService.saveAll(batteryRequests);
-            log.info("Data saved successfully.");
-        };
-    }
+//             batteryService.saveAll(batteryRequests);
+//             log.info("Data saved successfully.");
+//         };
+//     }
 }
